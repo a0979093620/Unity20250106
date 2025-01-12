9084c6e0188f6ad8c3642c636266ce8f55151e2f
Lotte <a0979093620@gmail.com>
2025-01-12 20:53:10 +0800

    Update-作業2

---
+++ Assets/Scripts/練習_2_資料類型.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class 練習_2_資料類型 : MonoBehaviour
+{
+    public float number_1 = 7.7f;       //新增公開變數_number_1 _浮點數_預設_7.7
+    public double numbe_2 = 0.12345d;   //新增公開變數_number_2_背經浮點數_預設0.12345   後綴詞為 d 或 D 
+    public decimal number_3 = 1.23456789m;  //新增公開變數_number_3_預設1.23456789   後綴詞為 m 或 M   不會顯示在清單
+}
