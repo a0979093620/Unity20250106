a9f4d4516322f4de6655c4abcaa3e255db986706
Lotte <a0979093620@gmail.com>
2025-01-13 19:15:45 +0800

    Update_整理註解

--- Assets/Practice/練習_1_變數.cs
+++ Assets/Practice/練習_1_變數.cs
@@ -4,6 +4,7 @@ public class 練習_1_變數 : MonoBehaviour
 {
     public int price = 999;                 //宣告公開變數_價格_預設_999 
     private float jump_height = 77.5f;      //宣告私人變數_跳躍高度_預設_77.5
-    public string weapen = "三項之力";      //宣告公開變數_武器_預設_三項之力
+    public string weapon = "三項之力";      //宣告公開變數_武器_預設_三項之力
     private bool Dragon_Spirit = true;      //宣告私人變數_龍魂_預設_是
 }
+ 
