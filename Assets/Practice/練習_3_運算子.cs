2eb6a796340b0d8844e0f31d4c9ba93a3539dfa7
Lotte <a0979093620@gmail.com>
2025-01-18 05:28:40 +0800

    Update_練習3、4的作業

---
+++ Assets/Practice/練習_3_運算子.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class 練習_3_運算子 : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log($"<color=orange>作業計算結果 : {6 / (2 * (1 + 2 ))}</color>");
+
+        Debug.Log($"<color=orange>用呼叫方法的方式算 : { Count()}</color>");
+    }
+
+    /// <summary>
+    /// 用別的方法嘗試
+    /// </summary>
+    [TextArea(1, 10)] public string Cal = "num1 / num2 (num3 + num4)";
+    [Header("數字1")] public int number1 = 6;
+    [Header("數字2")] public int number2 = 2;
+    [Header("數字3") , Range (1,10)] public int number3 = 1;
+    [ Header("數字4") , Range (1,10) ] public int number4 = 2;
+
+    /// <summary>
+    /// 方法設定
+    /// </summary>
+    /// <returns>計算結果</returns>
+     public int Count()
+    {
+        return number1 / (number2 *(number3 + number4));
+    }
+
+}
---
+++ Assets/Practice/練習_4_方法.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class 練習_4_方法 : MonoBehaviour 
+{
+    private void Awake()
+    {
+        Function_1();
+        Function_2();
+     
+        Debug.Log(Function_3());
+        Debug.Log(Function_4());
+    }
+
+    /// <summary>
+    /// 課堂作業
+    /// </summary>
+    public void Function_1()
+    {
+        Debug.Log(10 > 5); 
+    }
+
+    public void Function_2()
+    {
+        Debug.Log(10 < 5); 
+    }
+
+    /// <summary>
+    /// 用別的方法嘗試
+    /// </summary>
+    [TextArea(1, 10)] public string function_3 = "用function_3 的方法來判斷 number1 是否大於 number2";
+    [TextArea(1, 10)] public string function_4 = "用function_4 的方法來判斷 number1 是否小於 number2";
+    [Header("用來判斷的數字")]
+
+    //變數設定
+    public int number1 = 10;
+    public int number2 = 5;
+    private bool number3 = true;
+    private bool number4 = true;
+
+    /// <summary>
+    /// 其他嘗試
+    /// </summary>
+    /// <returns>判斷是否正確</returns>
+    public bool Function_3()
+    {
+        number3 = number1 > number2;
+        return number3;
+       
+    } public bool Function_4()
+    {
+        number4 = number1 < number2;
+        return number4; 
+     
+    }
+}
