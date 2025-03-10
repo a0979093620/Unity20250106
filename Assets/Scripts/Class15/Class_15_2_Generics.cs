a5b9fcc2255c1cb77895510165056bd0590a44cc
Lotte <a0979093620@gmail.com>
2025-03-10 21:42:49 +0800

    Update_泛型

--- Assets/Scripts/Class15/Class_15_2_Generics.cs
+++ Assets/Scripts/Class15/Class_15_2_Generics.cs
@@ -10,6 +10,7 @@ namespace lotta_15
     {
         private void Awake()
         {
+            #region 不使用泛型
             #region 數字
             int numberA = 7, numberB = 6;
             LogSystem.LogWithColor($"數字 A 與 B : {numberA} | {numberB}", "#f93");
@@ -28,17 +29,40 @@ namespace lotta_15
             object objectA = 3.5f, objectB = '好';
             LogSystem.LogWithColor($"物件 A 與 B : {objectA} | {objectB}", "#f33");
             SwapObject(ref objectA, ref objectB);
-            LogSystem.LogWithColor($"物件 A 與 B : {objectA} | {objectB}", "#f33"); 
+            LogSystem.LogWithColor($"物件 A 與 B : {objectA} | {objectB}", "#f33");
+            #endregion 
             #endregion
+
+            bool boolA = false, boolB = true;
+            LogSystem.LogWithColor($"物件 A 與 B : {boolA} | {boolB}", "#9f9");
+            Swap<bool>(ref boolA, ref boolB);
+            LogSystem.LogWithColor($"物件 A 與 B : {boolA} | {boolB}", "#9f9");
+
+            byte byteA = 1, byteB = 9;
+            LogSystem.LogWithColor($"物件 A 與 B : {byteA} | {byteB}", "#9f9");
+            Swap<byte>(ref byteA, ref byteB);
+            LogSystem.LogWithColor($"物件 A 與 B : {byteA} | {byteB}", "#9f9");
+
+
+
+            var player1 = new DataPlayer<int>();
+            player1.data = 99;
+            player1.LogData(123);
+
+            var player2 = new DataPlayer<string>();
+            player2.data = "玩家二號";
+            player2.LogData("哈囉");
+
 
         }
 
+        #region 不使用泛型的對調方法
         /// <summary>
         /// 數字對調
         /// </summary>
         /// <param name="a">第一個數字</param>
         /// <param name="b">第二個數字</param>
-        public void SwapNumber(ref int a , ref int b)       // ref 是同步功能，不用設定 a 或 b，直接將變數 numberA 和 numberB 同步成 a 和 b
+        public void SwapNumber(ref int a, ref int b)       // ref 是同步功能，不用設定 a 或 b，直接將變數 numberA 和 numberB 同步成 a 和 b
         {
             int temp = a;       // 將第一個數字放到旁邊
             a = b;              // 將第二個數字放到第一個
@@ -50,7 +74,7 @@ namespace lotta_15
         /// </summary>
         /// <param name="a">第一個字元</param>
         /// <param name="b">第二個字元</param>
-        public void SwapChar(ref char a,ref char b)
+        public void SwapChar(ref char a, ref char b)
         {
             char temp = a;
             a = b;
@@ -63,11 +87,35 @@ namespace lotta_15
         /// <param name="a">第一個物件</param>
         /// <param name="b">第二個物件</param>
         //不建議使用，會有裝箱拆箱的問題
-        public void SwapObject(ref object a,ref object b)
+        public void SwapObject(ref object a, ref object b)
         {
             object temp = a;
             a = b;
             b = temp;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 使用泛型對調資料
+        /// </summary>
+        /// <typeparam name="T">要對調的資料類型</typeparam>
+        /// <param name="a">第一個資料</param>
+        /// <param name="b">第二個資料</param>
+        public void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public class DataPlayer<T>
+        {
+            public T data;
+            public void LogData(T data)
+            {
+                LogSystem.LogWithColor(data, "#3ff");
+            }
         }
 
     }
