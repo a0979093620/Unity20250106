27d9e84b4d90a0517f62a8b82d49d4012056f79f
Lotte <a0979093620@gmail.com>
2025-03-12 19:51:24 +0800

    Update_資料結構

--- Assets/Scripts/Class15/Class_15_2_Generics.cs
+++ Assets/Scripts/Class15/Class_15_2_Generics.cs
@@ -6,7 +6,7 @@ namespace lotta_15
     /// <summary>
     /// 泛型
     /// </summary>
-    public class Class_15_2_Generics : MonoBehaviour
+    public class Class_15_2_Generics : MonoBehaviour, IClass_15_2_Generics
     {
         private void Awake()
         {
@@ -30,9 +30,10 @@ namespace lotta_15
             LogSystem.LogWithColor($"物件 A 與 B : {objectA} | {objectB}", "#f33");
             SwapObject(ref objectA, ref objectB);
             LogSystem.LogWithColor($"物件 A 與 B : {objectA} | {objectB}", "#f33");
-            #endregion 
+            #endregion
             #endregion
 
+            #region 泛型實例化
             bool boolA = false, boolB = true;
             LogSystem.LogWithColor($"物件 A 與 B : {boolA} | {boolB}", "#9f9");
             Swap<bool>(ref boolA, ref boolB);
@@ -52,8 +53,25 @@ namespace lotta_15
             var player2 = new DataPlayer<string>();
             player2.data = "玩家二號";
             player2.LogData("哈囉");
+            #endregion
 
 
+        }
+
+        private void Start()
+        {
+            var player = new Player();
+            var emeny = new Emeny();
+            var attackent = new AttackEvent<Player, Emeny>();
+            attackent.Attack(player, emeny);
+
+            var hp = new Hp();
+            var attack = new Attack();
+            attack.Increase(50);
+            hp.Increase(3.75f);
+
+            var checker = new CheckValue<Hp, float>();
+            checker.Check(hp);
         }
 
         #region 不使用泛型的對調方法
@@ -96,6 +114,7 @@ namespace lotta_15
 
         #endregion
 
+        #region 泛型
         /// <summary>
         /// 使用泛型對調資料
         /// </summary>
@@ -117,7 +136,64 @@ namespace lotta_15
                 LogSystem.LogWithColor(data, "#3ff");
             }
         }
+        #endregion
+
+        #region 泛型_兩種不同類型資料
+        // 泛型_兩種不同類型資料
+        public class Player { }
+        public class Emeny { }
+
+        public class AttackEvent<T, U>
+        {
+            public void Attack(T attacker, U defender)
+            {
+                LogSystem.LogWithColor($"{attacker} 攻擊 {defender}", "#f3f");
+            }
+        }
+        #endregion
+
+        #region 泛型介面
+        // 泛型介面
+        public interface IStat<T>
+        {
+            public T value { get; set; }        // 該狀態的值
+            public void Increase(T amount);     // 增加該狀態
+        }
+
+        public class Hp : IStat<float>
+        {
+            public float value { get; set; }
+            public void Increase(float amount)
+            {
+                value += amount;
+                LogSystem.LogWithColor($"血量 : {value}", "#f3f");
+            }
+        }
+
+        public class Attack : IStat<int>
+        {
+            public int value { get; set; }
+
+            public void Increase(int amount)
+            {
+                value += amount;
+                LogSystem.LogWithColor($"攻擊力 : {value}", "#f3f");
+            }
+        } 
+        #endregion
+
+        // 泛型 T 必須實作 IStat<U> 介面
+        // U 可以是任類型
+        public class CheckValue<T, U> where T : IStat<U>
+        {
+            public void Check(T stat)
+            {
+                // 添加約束後可使用 IStat<T> 成員
+                LogSystem.LogWithColor($"狀態的值 : {stat.value}", "#3d3");
+            }
+        }
 
     }
+
 }
 
---
+++ Assets/Scripts/Class15/IClass_15_2_Generics.cs
@@ -0,0 +1,10 @@
+﻿namespace lotta_15
+{
+    public interface IClass_15_2_Generics
+    {
+        void Swap<T>(ref T a, ref T b);
+        void SwapChar(ref char a, ref char b);
+        void SwapNumber(ref int a, ref int b);
+        void SwapObject(ref object a, ref object b);
+    }
+}
