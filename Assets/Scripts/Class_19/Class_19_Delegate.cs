803b942392fc4ff53e4850f53e91838705e62c6b
Lotte <a0979093620@gmail.com>
2025-03-19 21:44:29 +0800

    Update_泛型委派

--- Assets/Scripts/Class_19/Class_19_Delegate.cs
+++ Assets/Scripts/Class_19/Class_19_Delegate.cs
@@ -32,7 +32,15 @@ namespace lotta_19
         // 宣告一個無傳回與無參數的委派
         private delegate void DelegateMethod();
 
+        // 宣告有傳回有參數的委派
+        private delegate float Calculate(float numberA, float numberB);
+
+        // 泛型委派_無傳回有一個參數
+        private delegate void DelegateCombing<T>(T a);
+
+
         // 2. 目標方法
+        #region 無傳回無參數的方法
         private void Test()
         {
             LogSystem.LogWithColor("測試", "#f3f");
@@ -47,22 +55,79 @@ namespace lotta_19
         {
             LogSystem.LogWithColor("召喚小兵", "#3f3");
         }
+        #endregion
+
+        #region 有傳回有參數的方法
+        private float Add(float numberA, float numberB)
+        {
+            float result = numberA + numberB;
+            LogSystem.LogWithColor(result, "#7ff");
+            return result;
+        }
+
+        private float Sub(float numberA, float numberB)
+        {
+            float result = numberA - numberB;
+            LogSystem.LogWithColor(result, "#7ff");
+            return result;
+        }
+
+        private float Mul(float numberA, float numberB)
+        {
+            float result = numberA * numberB;
+            LogSystem.LogWithColor(result, "#7ff");
+            return result;
+        } 
+        #endregion
+
+        private void Combine<T>(T a)
+        {
+            LogSystem.LogWithColor(a, "#ff3");
+        }
 
         // 3. 宣告變數存放方法
         private DelegateMethod delegateMethod;
+        private Calculate calculate;
+
+        private DelegateCombing<float> delegateCombingFloat;
+        private DelegateCombing<int> delegateCombingInt;
 
         // 4. 呼叫委派
         private void Start()
         {
+            #region 無傳回無參數的委派
             delegateMethod = Test;          // 將方法 Test 存放到變數 delegateMethod 內
             delegateMethod += Talk;         // 多播委派 : 一個委派存放多個方法，要相同的簽章
             delegateMethod += MagicCard;    // 可添加多個方法
             delegateMethod -= Test;         // 也可以刪除方法
-            delegateMethod();           // 呼叫委派
+            delegateMethod();           // 呼叫委派  
+            #endregion
+
+            #region 有傳回有參數的委派
+            calculate += Add;
+            calculate += Sub;
+            calculate += Mul;
+            calculate(10, 3);
+            calculate(100, 70);
+            #endregion
+
+            CalcuteNunber(Sub, 3, 7);   //將方法當作參數傳遞
+
+            delegateCombingFloat = Combine<float>;
+            delegateCombingFloat(3.5f);
+            delegateCombingInt = Combine<int>;
+            delegateCombingInt(999);
+
+
+
         }
 
         // 委派 : 將方法當作參數
-
+        private void CalcuteNunber(Calculate calculate, float a, float b)
+        {
+            var result = calculate(a, b);
+            LogSystem.LogWithColor(result, "#ff3");
+        }
 
     }
 }
