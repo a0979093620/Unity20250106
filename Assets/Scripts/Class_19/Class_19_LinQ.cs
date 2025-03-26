8c56235865ac1221279547935208ac950332e12e
Lotte <a0979093620@gmail.com>
2025-03-26 19:16:47 +0800

    Update_LinQ

--- Assets/Scripts/Class_19/Class_19_LinQ.cs
+++ Assets/Scripts/Class_19/Class_19_LinQ.cs
@@ -1,5 +1,6 @@
 ﻿using lotta.tools;
 using System.Linq;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 namespace lotta_19
 {
@@ -10,9 +11,12 @@ namespace lotta_19
     public class Class_19_LinQ : MonoBehaviour
     {
         public int[] scores = { 90, 80, 77, 88, 93, 1, 5, 10, 35 };
-
+        public string[] items = { "紅色藥水", "藍色藥水", "地圖", "回家卷軸", "匕首", "紅色炸彈" };
+        public int[] numbers = { 1, 23, 77, 34, 42, 50, 80 };
+        
         private void Awake()
         {
+            #region LinQ基本用法
             // 使用 LinQ 的關鍵字查詢資料
             // 起手式 : 
             // from 資料來源
@@ -22,7 +26,7 @@ namespace lotta_19
             var queryAllData = from score in scores
                                select score;
 
-            foreach(var item in queryAllData)
+            foreach (var item in queryAllData)
             {
                 LogSystem.LogWithColor(item, "#3f3");
             }
@@ -35,7 +39,7 @@ namespace lotta_19
                              orderby score descending
                              select score;
 
-            foreach(var item in querySixty)
+            foreach (var item in querySixty)
             {
                 LogSystem.LogWithColor(item, "#ff3");
             }
@@ -46,16 +50,56 @@ namespace lotta_19
             var quaryGroup = from score in scores
                              group score by score % 2;
 
-            foreach(var group in quaryGroup)
+            foreach (var group in quaryGroup)
             {
                 LogSystem.LogWithColor(group, "#79f");
-                foreach(var item in group)
+                foreach (var item in group)
                 {
                     LogSystem.LogWithColor(item, "#79g");
                 }
 
                 LogSystem.LogWithColor("------群組分隔線-----", "#79f");
+            } 
+            #endregion
+
+            // let 中間變數，可以取得特定資料並儲存在變數內
+            var queryRed = from item in items
+                           let firstWord = items[0] //先取得所有資料的第一個字
+                           where firstWord == "紅"
+                           select item;
+
+            foreach (var item in queryRed)
+            {
+                LogSystem.LogWithColor($"第一個式紅的道具 : {item}", "#fa3");
             }
+
+            // into 將資料分組，在獲得資料時可透過 key 獲得分組名單
+            var queryGroup = from item in items
+                             group item by item[0] into newGroup
+                             select newGroup;
+
+            foreach (var group in queryGroup)
+            {
+                LogSystem.LogWithColor($"群組 : {group.Key}", "#3f3");
+                foreach(var item in group)
+                {
+                    LogSystem.LogWithColor($"道具 : {item}", "#9f9");
+                }
+            }
+
+            var queryEquals = from score in scores
+                              join number in numbers    // 加入第二組資料
+                              on score equals number    // on 比較兩組資料
+                              //select score;
+                              select number;            // 選兩組資料都相同
+            foreach(var item in queryEquals)
+            {
+                LogSystem.LogWithColor($"兩組相同的數字 : {item}", "#f33");
+            }
+            LogSystem.LogWithColor($"最大值 : {queryEquals.Max()}", "#7f7");
+            LogSystem.LogWithColor($"最小值 : {queryEquals.Min()}", "#7f7");
+            LogSystem.LogWithColor($"平均值 : {queryEquals.Average()}", "#7f7");
+            LogSystem.LogWithColor($"總和值 : {queryEquals.Sum()}", "#7f7");
 
         }
     }
